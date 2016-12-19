1323f3f3c1e80291895b5affaa8463354808da82
leeroy79 <lee.borlace@gmail.com>
2016-12-19 20:54:33 +0800

    Starting to automate the modification of zone.js

--- O365TaskRollupApp/gulpfile.js
+++ O365TaskRollupApp/gulpfile.js
@@ -1,24 +1,25 @@
-﻿/// <binding BeforeBuild='webpack:build-dev' />;
+﻿/// <binding BeforeBuild='zoneJsSPFix, webpack:build-dev' />
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var webpack = require('webpack');
 var webpackConfig = require('./webpack.config.js');
- 
+var replace = require('gulp-replace');
+
 gulp.task('default', function () {
     // place code for your default task here
 });
- 
+
 // Build and watch cycle (another option for development)
 // Advantage: No server required, can run app from filesystem
 // Disadvantage: Requests are not blocked until bundle is available,
 //               can serve an old app on refresh
 gulp.task('build-dev', ['webpack:build-dev'], function () {
-    gulp.watch(['app/**/*','app/*'], ['webpack:build-dev']);
+    gulp.watch(['app/**/*', 'app/*'], ['webpack:build-dev']);
 });
- 
+
 // create a single instance of the compiler to allow caching
 var devCompiler = webpack(webpackConfig);
- 
+
 gulp.task('webpack:build-dev', function (callback) {
     // run webpack
     devCompiler.run(function (err, stats) {
@@ -29,3 +30,12 @@ gulp.task('webpack:build-dev', function (callback) {
         callback();
     });
 });
+
+// Due to this bug with Zone.js : https://github.com/angular/zone.js/issues/434, we have to make a modification to Zone.js to avoid an issue.
+// We could manually edit the file whenever we get it via npm, but easier this way.
+gulp.task('zoneJsSPFix', function () {
+    //gulp.src(['node_modules/zone.js/dist/zone.js'])
+    //  .pipe(replace(/LEE123/g, 'CLARE123'))
+    //  .pipe(gulp.dest('zone.js'));
+});
+
